d6a467d746d0656f85b1af81c8c601d619065512
hugoht3 <hugoht3@hotmail.com>
2024-03-31 16:39:36 +0100

    added event listeners to java and used alerts for tests

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,5 +1,26 @@
 console.log("LOGGGED THIS")
 
+// Wait for the DOM finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!")
+            }else {
+                let gametype = this.getAttribute("data-type");
+                alert(`You clicked ${gametype}`);
+            }
+
+        });
+    }
+});
+
+
 
 function runGame()  {
 
