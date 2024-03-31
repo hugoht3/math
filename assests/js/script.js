98d04aa00cfb4a2cd85e15b2a2cda2a2bbe0138c
hugoht3 <hugoht3@hotmail.com>
2024-03-31 16:24:14 +0100

    added all empytied functions to the js file

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,1 +1,32 @@
 console.log("LOGGGED THIS")
+
+
+function runGame()  {
+
+};
+
+
+function checkAnswer () {
+
+};
+
+function calculateCorrectAnswer () {
+
+};
+
+function incrementScore () {
+
+};
+
+function displayAdditionQuestion () {
+
+};
+
+function displaySubtractQuestion () {
+
+};
+
+function displayMultiplyQuestion () {
+
+};
+
